7949df2fc5f63331384a24f770ace0292c7ab604
GoldLion1011 <yogirose79@gmail.com>
2022-11-30 10:26:52 -0800

    tasks 0 and 1

---
+++ 0x0E-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Returns information about and employee's TODO list progress """
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + '/users/{}'.format(sys.argv[1])).json()
+    user_id = sys.argv[1]
+    user_name = user.get('username')
+    todos = requests.get(url + "todos", params={'userId': user_id}).json()
+
+    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, user_name, t.get('completed'), t.get('title')]
+        ) for t in todos]
