88c856b985607b66c5d3d589d14180b91e3f19c5
GoldLion1011 <yogirose79@gmail.com>
2022-11-30 12:17:54 -0800

    task 1 redo

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
 """ Returns information about and employee's TODO list progress """
 import csv
-import json
 import requests
 import sys
 
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + '/users/{}'.format(sys.argv[1])).json()
-    user_id = sys.argv[1]
-    user_name = user.get('username')
-    todos = requests.get(url + "todos", params={'userId': user_id}).json()
+def get_u_name(url, user_id):
+    """ Returns user name/employee """
+    response = requests.get('{}users/{}'.format(url, user_id))
+    u_dict = response.json()
+    return u_dict['name']
 
-    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
+
+def get_todos(url, user_id):
+    """ Returns user's todo list progress """
+    response = requests.get('{}users/{}/todos'.format(url, user_id))
+    return response.json()
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+
+    u_name = get_u_name(url, user_id)
+    todo_list = get_todos(url, user_id)
+
+    with open('{}.csv'.format(user_id), 'w') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, user_name, t.get('completed'), t.get('title')]
-        ) for t in todos]
+        for todo in todo_list:
+            writer.writerow(
+                [user_id, u_name, todo['completed'], todo['title']])
